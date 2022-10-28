ee796140bf3f8b0cf5eb9a995242af17a13a64fa
Saiteju1997 <51901826+Saiteju1997@users.noreply.github.com>
2022-10-28 12:46:39 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 0.13.5"
+
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = ">= 2.1"
+    }
+  }
+}
+
+resource "null_resource" "consulrollout" {
+  provisioner "local-exec" {
+    command = "echo ${var.git_hash}"
+  }
+}
+
+variable "git_hash" {
+  type = string
+}
